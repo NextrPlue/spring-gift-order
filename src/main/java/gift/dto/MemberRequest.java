6cd6888329d60c66b51dad4d49189886b4959891
NextrPlue <p.plue1881@gmail.com>
2024-07-30 17:48:51 +0900

    refactor(dto): Refactor MemberRequest record for Kakao data transfer

--- src/main/java/gift/dto/MemberRequest.java
+++ src/main/java/gift/dto/MemberRequest.java
@@ -1,9 +1,3 @@
 package gift.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberRequest(
-        @NotBlank @Email String email,
-        @NotBlank String password
-) {}
+public record MemberRequest(Long kakaoId, String nickname, String kakaoToken) {}
