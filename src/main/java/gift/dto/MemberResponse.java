71d3d9e50742b2e0b007c3d39a039281636440ae
NextrPlue <p.plue1881@gmail.com>
2024-07-30 17:49:10 +0900

    refactor(dto): Refactor MemberResponse class for Kakao data transfer

--- src/main/java/gift/dto/MemberResponse.java
+++ src/main/java/gift/dto/MemberResponse.java
@@ -4,17 +4,16 @@ import java.util.List;
 
 public class MemberResponse {
     private Long id;
-    private String email;
-    private String token;
+    private Long kakaoId;
+    private String nickname;
+    private String kakaoToken;
     private List<WishResponse> wishes;
 
-    public MemberResponse(String token) {
-        this.token = token;
-    }
-
-    public MemberResponse(Long id, String email, List<WishResponse> wishes) {
+    public MemberResponse(Long id, Long kakaoId, String nickname, String kakaoToken, List<WishResponse> wishes) {
         this.id = id;
-        this.email = email;
+        this.kakaoId = kakaoId;
+        this.nickname = nickname;
+        this.kakaoToken = kakaoToken;
         this.wishes = wishes;
     }
 
@@ -22,31 +21,19 @@ public class MemberResponse {
         return id;
     }
 
-    public String getEmail() {
-        return email;
+    public Long getKakaoId() {
+        return kakaoId;
     }
 
-    public String getToken() {
-        return token;
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getKakaoToken() {
+        return kakaoToken;
     }
 
     public List<WishResponse> getWishes() {
         return wishes;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public void setWishes(List<WishResponse> wishes) {
-        this.wishes = wishes;
     }
 }
